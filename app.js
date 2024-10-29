e800f71cf8b6358184d47b026a390fe208787e4d
Evgenii Pasechnickov <evgenii.pase4nickov@gmail.com>
2024-10-29 15:31:19 +0300

    Homework #2

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-function sayHello(){
-    console.log("Привет, мир!");
-}
+//создание String
+let a = '1';
 
-function sayName(name){
-    console.log(`Привет, ${name}!`);
-    console.log("Пункт №9");
-}
+let A = String();
+
+const NEW_STRING = 0 + ' ';
+
+//создание Number
+
+let b = 1;
+
+let B = Number();
+
+const NEW_NUMBER = +'1';
+
+//создание Boolean
+
+let c = 1;
+c = !!c;
+
+let C = Boolean();
+
+const NEW_BOOLEAN = true;
+
+//создание Null
+
+let d = null;
+
+//создание Undefined
+
+let e;
+
+const NEW_UNDEFINED = {a: 3}
+NEW_UNDEFINED.b
+
+//создание Symbol
+
+let f = Symbol('test')
+
+const NEW_SYMBOL = Symbol('TEST')
+
+//создание BigInt
+
+let g = BigInt('1234567');
+
+let G = BigInt(10);
+
+const NEW_BIGINT = 12n;
